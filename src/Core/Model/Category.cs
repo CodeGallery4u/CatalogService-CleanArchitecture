7524a626fc0c0c1e29be61d0c65ee55bfa4b63c7
Safder <mohd.safder.sf24@gmail.com>
2022-10-16 22:13:40 +0530

    domain models were added.

---
+++ src/Core/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class Category : BaseEntity
+    {
+        public Category ParentCategory { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
